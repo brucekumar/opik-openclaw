64582f2bdbd22eabf705c9f87096cf29601d80b4
Vincent Koc <vincentkoc@ieee.org>
2026-03-02 11:38:30 -0800

    refactor: resolve opik config from pluginConfig

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { emptyPluginConfigSchema } from "openclaw/plugin-sdk";
 import { disableLogger } from "opik";
 import { registerOpikCli } from "./src/configure.js";
 import { createOpikService } from "./src/service.js";
+import { parseOpikPluginConfig } from "./src/types.js";
 
 // Suppress Opik SDK tslog console output
 disableLogger();
@@ -13,7 +14,8 @@ const plugin = {
   description: "Export LLM traces and spans to Opik for observability",
   configSchema: emptyPluginConfigSchema(),
   register(api: OpenClawPluginApi) {
-    api.registerService(createOpikService(api));
+    const pluginConfig = parseOpikPluginConfig(api.pluginConfig);
+    api.registerService(createOpikService(api, pluginConfig));
     api.registerCli(
       ({ program }) =>
         registerOpikCli({
